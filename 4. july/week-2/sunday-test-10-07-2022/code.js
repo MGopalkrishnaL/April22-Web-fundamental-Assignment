81d246647f56c260902a6f6ad9c9d9c6e5c38bca
M Gopal Krishna <96627837+MGopalkrishnaL@users.noreply.github.com>
2022-07-11 14:51:24 +0530

    Update code.js

--- 4. july/week-2/sunday-test-10-07-2022/code.js
+++ 4. july/week-2/sunday-test-10-07-2022/code.js
@@ -10,7 +10,9 @@ function getchange() {
                 <h4 id="name">${currentVale.name}</h4>
         <h4 id="email">${currentVale.email}</h4>
         <h4 id="city">${currentVale.address.city}</h4>
-        <h4 id="website">${currentVale.website}</h4>`;
+        <h4 id="website">${currentVale.website}</h4>
+        <button> FETCH DATA </button>
+        `;
       });
       document.getElementById("card").innerHTML = data1;
     });
